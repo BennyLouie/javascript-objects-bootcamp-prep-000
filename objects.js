0592e78d967a4f42148e9e0502b2776514090b8c
Benny Louie <bennylouie1412@gmail.com>
2019-06-13 20:25:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
-var playlist = {artist: song}
+var playlist = {artist: song};
 
 function updatePlaylist(playlist, artist, song) {
   playlist.artist = song
-  return playlist
+  return playlist;
 }
+
