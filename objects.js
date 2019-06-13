706d251febb092ed8ef17166e3525d6930e8a379
Benny Louie <bennylouie1412@gmail.com>
2019-06-13 20:47:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,10 @@ var playlist = {};
 var playlist = {'Adele': 'Hello', 'Justin Beiber': 'What Do You Mean', 'Taylor Swift': 'Love Story'};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
-  return playlist
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist;
 }
