e8c83e33457f9df8d1644907ef93e195c20dabd6
Benny Louie <bennylouie1412@gmail.com>
2019-06-13 20:41:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {};
 var playlist = {'Adele': 'Hello', 'Justin Beiber': 'What Do You Mean', 'Taylor Swift': 'Love Story'};
-function updatePlaylist(obj, key, value) {
-  obj.key(value)
-  return obj
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName(songTitle)
+  return playlist
 }
